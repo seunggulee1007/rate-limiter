435745a2722ca3160a6291f14288fd2b9f2590f4
이승구 <leesg107@naver.com>
2024-07-23 15:07:37 +0900

    test: token bucket 테스트

---
+++ src/test/java/com/yeseung/ratelimiter/car/controller/ParkingV1ControllerTest.java
@@ -0,0 +1,69 @@
+package com.yeseung.ratelimiter.car.controller;
+
+import com.yeseung.ratelimiter.car.controller.request.ParkingApplyRequest;
+import com.yeseung.ratelimiter.common.exceptions.RateLimitException;
+import com.yeseung.ratelimiter.common.properties.TokenBucketProperties;
+import com.yeseung.ratelimiter.container.RedisTestContainer;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.testcontainers.shaded.com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.junit.jupiter.api.Assertions.assertInstanceOf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+class ParkingV1ControllerTest extends RedisTestContainer {
+
+    private static final Logger log = LoggerFactory.getLogger(ParkingV1ControllerTest.class);
+    @Autowired
+    protected MockMvc mockMvc;
+    @Autowired
+    private TokenBucketProperties tokenBucketProperties;
+
+    @Test
+    @DisplayName("주차권 신청 처리율 제한 테스트")
+    void test() throws Exception {
+        ObjectMapper objectMapper = new ObjectMapper();
+        // given
+        ParkingApplyRequest carInfo = new ParkingApplyRequest("07로3725", "seunggulee", "20240722", "10", "00");
+        String s = objectMapper.writeValueAsString(carInfo);
+        int capacity = tokenBucketProperties.getCapacity();
+        int count = capacity + 2;
+        for (int i = 0; i < count; i++) {
+            confirm_remaining(s, capacity - i - 1);
+        }
+        log.error("s: {}", s);
+        // when
+
+        // then
+
+    }
+
+    private void confirm_remaining(String parkingInfo, int count) throws Exception {
+        if (count >= 0) {
+            mockMvc.perform(post("/api/v1/car/parking").contentType(MediaType.APPLICATION_JSON).content(parkingInfo))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(header().string("X-RateLimit-Remaining", String.valueOf(count)))
+            ;
+        } else {
+
+            mockMvc.perform(post("/api/v1/car/parking").contentType(MediaType.APPLICATION_JSON).content(parkingInfo))
+                .andDo(print())
+                .andExpect(status().isTooManyRequests())
+                .andExpect(result -> assertInstanceOf(RateLimitException.class,
+                                                      result.getResolvedException()));
+        }
+
+    }
+
+}
